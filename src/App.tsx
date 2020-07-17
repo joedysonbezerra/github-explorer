b4a6cc70e370e84882488fef8c13ab6f2b1f2a41
Joedyson Bezerra <joedysonbezerra@gmail.com>
2020-07-17 16:15:45 -0300

    projeto finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import GlobalStyle from './styles/global';
+import Routes from './routes';
 
-function App () {
-  return <h1>Hello World</h1>
-}
+const App: React.FC = () => (
+  <>
+    <GlobalStyle />
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </>
+);
 
-export default App
+export default App;
